42f8315bdb702afa6b927b37003cf35c80724e0d
Joe-noh <honzawa.j@pepabo.com>
2015-06-16 11:19:13 +0900

    verify friendly forwarding forwards only first time

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -24,6 +24,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as @user
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
 
     name  = "Foo Bar"
     email = "foo@bar.com"
