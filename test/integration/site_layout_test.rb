6f2caed9bcd928d04e972a07a4e49ce8b927b6a7
Joe-noh <honzawa.j@pepabo.com>
2015-06-16 12:08:06 +0900

    add several layout assertion

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
+
+    assert_select "a[href=?]", users_path,  count: 0
+    assert_select "a[href=?]", logout_path, count: 0
   end
 
   test "layout links (logged in)" do
