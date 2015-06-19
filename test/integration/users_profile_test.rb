5737e184845fb446562a7e6064646c8b3dc9c4d9
Joe-noh <honzawa.j@pepabo.com>
2015-06-19 17:44:13 +0900

    verify that stats is correctly displayed

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -21,4 +21,14 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
   end
+
+  test "stats display" do
+    get user_path(@user)
+
+    assert_select "div.stats"
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_select "a[href=?]", followers_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+  end
 end
