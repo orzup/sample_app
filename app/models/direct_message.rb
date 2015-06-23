79ffaebc045918b4a1c0693112caad5a403af4d0
Joe-noh <honzawa.j@pepabo.com>
2015-06-23 17:34:28 +0900

    define DirectMessage model

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -1,2 +1,10 @@
 class DirectMessage < ActiveRecord::Base
+  belongs_to :from_user, class_name: "User"
+  belongs_to :to_user,   class_name: "User"
+
+  default_scope -> {order created_at: :desc}
+
+  validates :content, presence: true, length: {maximum: 10000}
+  validates :from_user_id, presence: true
+  validates :to_user_id,   presence: true
 end
--- test/models/direct_message_test.rb
+++ test/models/direct_message_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class DirectMessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @one = direct_messages(:one)
+    @two = direct_messages(:two)
+  end
+
+  test "should be valid" do
+    assert @one.valid?
+    assert @two.valid?
+  end
+
+  test "should have content" do
+    @one.content = "   "
+    assert_not @one.valid?
+  end
+
+  test "content should be at most 10000 characters" do
+    @one.content = "a"*10000
+    assert @one.valid?
+
+    @one.content = "a"*10001
+    assert_not @one.valid?
+  end
+
+  test "should have from_user_id" do
+    @one.from_user_id = nil
+    assert_not @one.valid?
+  end
+
+  test "should have to_user_id" do
+    @one.to_user_id = nil
+    assert_not @one.valid?
+  end
+
+  test "users relation" do
+    assert @one.from_user == users(:michael)
+    assert @one.to_user   == users(:lana)
+  end
 end
