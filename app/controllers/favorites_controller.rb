b03ffc33f1386f8639f2044af9169cc086727dae
Joe-noh <honzawa.j@pepabo.com>
2015-06-23 11:35:15 +0900

    suzupyの指摘を反映
    Favしたときにjsで総Fav数を変更していたが、
    他人のプロフィールを開いているときにFavすると
    そのユーザの総Fav数を上書きしてしまう不具合を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,23 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @user      = current_user
     @micropost = Micropost.find(params[:micropost_id])
 
-    @user.favor(@micropost)
+    current_user.favor(@micropost)
     respond_to do |format|
-      format.html { redirect_back_or @user }
+      format.html { redirect_back_or @micropost.user }
       format.js
     end
   end
 
   def destroy
-    @user      = current_user
     @micropost = Favorite.find(params[:id]).micropost
 
-    @user.unfavor(@micropost)
+    current_user.unfavor(@micropost)
     respond_to do |format|
-      format.html { redirect_back_or @user }
+      format.html { redirect_back_or @micropost.user }
       format.js
     end
   end
