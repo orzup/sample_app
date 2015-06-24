98a8473fa575e0811a0202bdfdee174b956c6b08
Joe-noh <honzawa.j@pepabo.com>
2015-06-24 12:08:00 +0900

    add a link to DM on header

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,8 +12,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", signup_path
 
-    assert_select "a[href=?]", users_path,  count: 0
-    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", users_path,           count: 0
+    assert_select "a[href=?]", direct_messages_path, count: 0
+    assert_select "a[href=?]", logout_path,          count: 0
   end
 
   test "layout links (logged in)" do
@@ -29,11 +30,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", direct_messages_path
     assert_select "a[href=?]", edit_user_path(user)
     assert_select "a[href=?]", logout_path
   end
 
-  test "layout links (logged-in)" do
+  test "stats links (logged-in)" do
     @user = users(:michael)
     log_in_as @user
     get root_path
