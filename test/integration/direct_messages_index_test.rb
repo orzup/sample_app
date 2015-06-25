d2f2aa53aa67a9aec237cf7029c3897d6a763296
Joe-noh <honzawa.j@pepabo.com>
2015-06-25 15:54:53 +0900

    DM画面のテストを追加

--- test/integration/direct_messages_index_test.rb
+++ test/integration/direct_messages_index_test.rb
@@ -3,14 +3,19 @@ require 'test_helper'
 class DirectMessagesIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @friend = users(:lana)
+    @other = users(:archer)
     log_in_as @user
   end
 
-  test "listed users should follow michael and michael follows him/her" do
+  test "listed user and michael follow each other or talked before" do
     get direct_messages_path
 
-    assert assigns(:target_users).all? {|user| user.following? @user }
-    assert assigns(:target_users).all? {|user| @user.following? user }
+    assert assigns(:target_users).all? do |user|
+      (user.following?(@user) && @user.following?(user))||
+      DirectMessage.where(from_user:  user, to_user: @user).count > 0 ||
+      DirectMessage.where(from_user: @user, to_user:  user).count > 0
+    end
   end
 
   test "links to talk page should be there" do
@@ -19,5 +24,15 @@ class DirectMessagesIndexTest < ActionDispatch::IntegrationTest
     assigns(:target_users).each do |user|
       assert_select "a[href=?]", direct_messages_talk_path(user)
     end
+  end
+
+  test "mutual friend DM page should have message form" do
+    get direct_messages_talk_path(@friend)
+    assert_select "form[id=?]", "new_direct_message"
+  end
+
+  test "non-mutual friend DM page should not have message form" do
+    get direct_messages_talk_path(@other)
+    assert_select "form[id=?]", "new_direct_message", count: 0
   end
 end
